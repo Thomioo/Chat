bc047dc3b16e773c33bff5f1369f8c85d0b4b4ca
Thomioo <88208504+Thomioo@users.noreply.github.com>
2024-01-03 17:22:05 +0100

    Create main.py

---
+++ Server/main.py
@@ -0,0 +1,62 @@
+import uvicorn, json, datetime
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime
+
+app = FastAPI()
+
+reloaded = False
+
+# Set up CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+try:
+    with open("msgs.json", "r") as file:
+        data = json.loads(file.read())
+except:
+    data = {}
+
+@app.get("/")
+def f(key: int):
+    if key == 86923:
+        return JSONResponse(dict(list(data.items())[:100]), 200)
+    
+    if datetime.minute % 10 == 0 and reloaded == False:
+        # reload
+        with open("msgs.json", "w+") as f:
+            f.write(json.dumps(data))
+        reloaded = True
+    else:
+        reloaded = False
+    return 404
+
+
+@app.get("/message")
+def f(user: str, message: str, time: str):
+    try:
+        last = int(list(data)[-1]) + 1
+    except:
+        last = 0
+    data.update({
+        last: {
+            "user": user,
+            "message": message, 
+            "time": time,
+            }
+        }
+    )
+    return JSONResponse(dict(list(data.items())[:100]), 200)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080)
+    with open("msgs.json", "w+") as f:
+        f.write(json.dumps(data))
